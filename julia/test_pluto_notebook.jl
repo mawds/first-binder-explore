7106a61cabebc6a538390f6e36771d85adda40bc
David Mawdsley <david.mawdsley@manchester.ac.uk>
2022-05-30 09:52:45 +0100

    Add example Pluto notebook

---
+++ julia/test_pluto_notebook.jl
@@ -0,0 +1,40 @@
+### A Pluto.jl notebook ###
+# v0.19.5
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ b892d9ad-e635-4270-8f15-629f23b786d2
+md"""
+# Hello world
+This is a test
+"""
+
+
+# ╔═╡ 9b94693b-5219-4558-923b-361607d6268b
+1 + 1
+
+# ╔═╡ c926e43f-8015-4c15-a92d-22812cdea5f2
+
+
+# ╔═╡ 00000000-0000-0000-0000-000000000001
+PLUTO_PROJECT_TOML_CONTENTS = """
+[deps]
+"""
+
+# ╔═╡ 00000000-0000-0000-0000-000000000002
+PLUTO_MANIFEST_TOML_CONTENTS = """
+# This file is machine-generated - editing it directly is not advised
+
+julia_version = "1.7.2"
+manifest_format = "2.0"
+
+[deps]
+"""
+
+# ╔═╡ Cell order:
+# ╟─b892d9ad-e635-4270-8f15-629f23b786d2
+# ╠═9b94693b-5219-4558-923b-361607d6268b
+# ╠═c926e43f-8015-4c15-a92d-22812cdea5f2
+# ╟─00000000-0000-0000-0000-000000000001
+# ╟─00000000-0000-0000-0000-000000000002
---
+++ julia/testpluto/src/testpluto.jl
@@ -0,0 +1,5 @@
+module testpluto
+
+greet() = print("Hello World!")
+
+end # module
